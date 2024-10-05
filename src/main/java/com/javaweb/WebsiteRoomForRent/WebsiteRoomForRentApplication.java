e8f77353f40d0b34839aa0f05b03cdd08601a672
quocbao2004 <daoquocbao2k04@gmail.com>
2024-10-05 09:44:52 +0700

    init project

---
+++ src/main/java/com/javaweb/WebsiteRoomForRent/WebsiteRoomForRentApplication.java
@@ -0,0 +1,13 @@
+package com.javaweb.WebsiteRoomForRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteRoomForRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteRoomForRentApplication.class, args);
+	}
+
+}
